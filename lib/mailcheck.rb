0f06f0853a72a80bb07c4b7f2b7ebce645913b07
Martin Ferrari <dmferrari@gmail.com>
2017-09-04 18:34:31 -0300

    Linter applied

--- lib/mailcheck.rb
+++ lib/mailcheck.rb
@@ -1,7 +1,6 @@
-require "mailcheck/version"
+require 'mailcheck/version'
 
 class Mailcheck
-
   THRESHOLD = 3
 
   DOMAINS = [
@@ -95,7 +94,7 @@ class Mailcheck
     if closest_domain
       if closest_domain != email_parts[:domain]
         # The email address closely matches one of the supplied domains return a suggestion
-        return { :address => email_parts[:address], :domain => closest_domain, :full => "#{email_parts[:address]}@#{closest_domain}" }
+        return { address: email_parts[:address], domain: closest_domain, full: "#{email_parts[:address]}@#{closest_domain}" }
       end
     else
       # The email address does not closely match one of the supplied domains
@@ -104,7 +103,7 @@ class Mailcheck
         # The email address may have a mispelled top-level domain return a suggestion
         domain = email_parts[:domain]
         closest_domain = closest_domain_for(email_parts, domain, closest_top_level_domain)
-        return { :address => email_parts[:address], :domain => closest_domain, :full => "#{email_parts[:address]}@#{closest_domain}" }
+        return { address: email_parts[:address], domain: closest_domain, full: "#{email_parts[:address]}@#{closest_domain}" }
       end
     end
     # The email address exactly matches one of the supplied domains, does not closely
@@ -116,8 +115,7 @@ class Mailcheck
   def find_closest_domain(domain, domains)
     min_dist = 99
     closest_domain = nil
-    return nil if domains.nil? || domains.size == 0
-
+    return nil if domains.nil? || domains.empty?
     domains.each do |dmn|
       return domain if domain == dmn
       dist = sift_3distance(domain, dmn)
@@ -126,29 +124,18 @@ class Mailcheck
         closest_domain = dmn
       end
     end
-
-    if min_dist <= THRESHOLD && closest_domain
-      closest_domain
-    else
-      nil
-    end
+    closest_domain if min_dist <= THRESHOLD && closest_domain
   end
 
   def sift_3distance(s1, s2)
     # sift3: http:#siderite.blogspot.com/2007/04/super-fast-and-accurate-string-distance.html
-
-    c = 0
-    offset1 = 0
-    offset2 = 0
-    lcs = 0
+    c = lcs = offset1 = offset2 = 0
     max_offset = 5
-
-    while (c + offset1 < s1.length) && (c + offset2 < s2.length) do
+    while (c + offset1 < s1.length) && (c + offset2 < s2.length)
       if s1[c + offset1] == s2[c + offset2]
         lcs += 1
       else
-        offset1 = 0
-        offset2 = 0
+        offset1 = offset2 = 0
         max_offset.times do |i|
           if c + i < s1.length && s1[c + i] == s2[c]
             offset1 = i
@@ -167,18 +154,14 @@ class Mailcheck
 
   def split_email(email)
     parts = email.split('@')
-
-    return false if parts.length < 2 || parts.any?{ |p| p == '' }
-
+    return false if parts.length < 2 || parts.any? { |p| p == '' }
     domain = parts.pop
     domain_parts = domain.split('.')
-
-    return false if domain_parts.length == 0
-
+    return false if domain_parts.empty?
     {
-      :top_level_domain => domain_parts[1..-1].join('.'),
-      :domain => domain,
-      :address => parts.first
+      top_level_domain: domain_parts[1..-1].join('.'),
+      domain: domain,
+      address: parts.first
     }
   end
 
